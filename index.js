b561f4a763d5ba2dbc76fa3fdcae7d87d6895ea9
laxmi-2543 <lakshmi.rakkaiah@gmil.com>
2023-08-04 11:33:12 +0530

    secondcommit

--- index.js
+++ index.js
@@ -269,5 +269,5 @@ app.get("/student/:studentId/previousMentor", async (req, res) => {
 /*------------------------------------------------------------*/
 
 app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
+  console.log("server is running");
 });
